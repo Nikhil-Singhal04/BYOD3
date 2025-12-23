8ef468b07dc778d51178ab529efefc2df15aef81
Nikhil Singhal <nikhilsinghal2019@gmail.com>
2025-12-23 17:57:54 +0530

    add files

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b233c98b585ba" # Replace with a suitable AMI
+  ami           = "ami-0ecb62995f68bb549" # Replace with a suitable AMI
   instance_type = var.instance_type # Use the variable for instance type
 
   tags = {
